7da6ea5f7df954ec9aa707c35d1d555b5a4a70b4
liamcclane <lia_mcclane@msn.com>
2020-02-03 17:55:46 -0800

    one algo a day

--- easy/twoSums.js
+++ easy/twoSums.js
@@ -10,8 +10,17 @@
 
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
-function twoSums(arr,target){
-    let x = 0, y =1; // initatlizing for the indicies we are looking for
-    
-    return [x,y]
-}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+};
