f40657ce2506f7bef3159a22c33e99f147f5ccd1
liamcclane <lia_mcclane@msn.com>
2020-02-06 12:49:17 -0800

    an aglo and day

---
+++ easy/countAndSay.js
@@ -0,0 +1,51 @@
+/**
+ * The count-and-say sequence is the sequence of integers with the first five terms as following:
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 1 is read off as "one 1" or 11.
+ * 11 is read off as "two 1s" or 21.
+ * 21 is read off as "one 2, then one 1" or 1211.
+ * Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. 
+ * You can do so recursively, in other words from the previous member read off the digits, 
+ * counting the number of digits in groups of the same digit.
+ * Note: Each term of the sequence of integers will be represented as a string.
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221 
+ * 6.     312211
+ * 7.     13112221
+ * 8.     1113213211
+ * 9.     31131211131221
+ * 10.     13211311123113112211
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    return countNsay(n);
+};
+
+function countNsay(count, str){
+  if(!str) { str = "1"; }
+  if(count === 0) { return "0"; }
+  if(count === 1) { return str; }
+  
+  let char = str[0];
+  let charCount = 1;
+  let newStr = "";
+  for(let i = 1; i < str.length; i++){
+    if(char === str[i]){
+      charCount++;
+    }else{
+      newStr += charCount + "" + char;
+      char = str[i];
+      charCount = 1;
+    }
+  }
+  newStr += charCount + "" + char;
+  return countNsay(count-1, newStr);
+}
---
+++ easy/jewelsAndStones.js
@@ -0,0 +1,38 @@
+/** 
+ * You're given strings J representing the types of stones that are jewels, 
+ * and S representing the stones you have.  
+ * Each character in S is a type of stone you have.  
+ * You want to know how many of the stones you have are also jewels.
+ * 
+ * The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+ * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ * Example 1:
+ * Input: J = "aA", S = "aAAbbbb"
+ * Output: 3
+ * Example 2:
+ * Input: J = "z", S = "ZZ"
+ * Output: 0
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    // creating a dictoary of jewels to cross check if any of your stones are Jewels
+    let jewels = {};
+    // ctreating a count to increase whenever you find out that your stone is in fact a Jewel
+    let count = 0;
+    
+    // creating the dictonary
+    for(let jewel of J){
+        if(jewel in jewels) jewels[jewel] ++;
+        else jewels[jewel] = 0;
+    }
+    
+    // looping though your stones
+    for(let stone of S){
+        if(stone in jewels) count ++;
+    }
+    
+    // returning the result
+    return count;
+};
