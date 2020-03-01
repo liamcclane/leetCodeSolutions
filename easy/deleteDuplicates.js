a3d499588b138126ea7c543ee9fef22fe6463f4c
liamcclane <lia_mcclane@msn.com>
2020-02-29 19:49:24 -0800

    keeps unemployment away

--- easy/deleteDuplicates.js
+++ easy/deleteDuplicates.js
@@ -6,16 +6,37 @@
  * }
  */
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function (head) {
-    let prev, curr = head;
-    if (head === null) return head;
-    while (curr != null) {
-        console.log(curr.val);
-        prev = curr;
-        curr = curr.next;
-    }
+var deleteDuplicates = function(head) {
+    if(head === null) return head;
+    let curr = head;
+    if(curr.next === null) return head;
     
+    curr = curr.next;
+    prev = head;
+    while(curr != null){
+        
+        if( prev.val != curr.val ){
+            prev = curr;
+            curr = curr.next;
+         } else {
+             hold = prev;
+             while(prev.val === curr.val){
+                 prev = curr;
+                 curr = curr.next;
+             }
+             hold.next = curr;
+         }
+        // console.log(`prev.val ${prev.val}`);
+        // curr = curr.next;
+    }
 };
