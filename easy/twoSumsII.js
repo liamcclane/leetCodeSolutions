057bc5121bdd9033993ed106bff0d72bb1e066dd
liamcclane <lia_mcclane@msn.com>
2020-03-12 20:13:09 -0700

    an algo a day

---
+++ easy/twoSumsII.js
@@ -0,0 +1,58 @@
+/**
+ * Given an array of integers that is already sorted in ascending order, 
+ * find two numbers such that they add up to a specific target number.
+ * 
+ * The function twoSum should return indices of the two numbers such 
+ * that they add up to the target, where index1 must be less than index2.
+ * 
+ * Note:
+ * 
+ * Your returned answers (both index1 and index2) are not zero-based.
+ * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ * Example:
+ * 
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        let excluded = numbers.slice(0, i).concat(numbers.slice(i + 1, numbers.length));
+        // console.log(excluded);
+        if (excluded.includes(target - numbers[i])) {
+            if (numbers.indexOf(target - numbers[i]) + 1 === i + 1) {
+                // console.log("inside here");
+                // console.log(numbers.indexOf(target - numbers[i], 2));
+                return [i + 1, numbers.indexOf(target - numbers[i], 2) + 2]
+            }
+            return [i + 1, numbers.indexOf(target - numbers[i]) + 1];
+        }
+    }
+};
+// console.log(twoSum([1, 2, 5, 7, 11, 15], 9)); // [1,2]
+// console.log()
+// console.log(twoSum([2, 3, 7, 11, 15], 9)); // [1,3]
+// console.log()
+// console.log(twoSum([0, 0, 3, 4], 0)); // [1,3]
+console.log(twoSum([1,2,3,4,4,9,56,90],8))
+// console.log([3,4,5,0,6,6,60,0,7,6].lastIndexOf(0));
+// console.log("Darrick solution")
+
+
+var twoSumDarrick = function (numbers, target) {
+    let myMap = new Map();
+    for (let i = 0; i < numbers.length; i++) {
+        let compliement = target - numbers[i];
+        if (myMap.has(compliement)) {
+            return [myMap.get(compliement) + 1, i + 1];
+        }
+        myMap.set(numbers[i] + 1, i + 1);
+        console.log(myMap);
+    }
+};
+// console.log(twoSumDarrick([2, 7, 11, 15], 9)); // [1,2]
+// console.log()
+// console.log(twoSumDarrick([2, 3, 7, 11, 15], 9)); // [1,3]
--- hard/missingPositive.js
+++ hard/missingPositive.js
@@ -1,6 +1,6 @@
 /**
- * Runtime: 60 ms, faster than 56.49% of JavaScript online submissions for First Missing Positive.
- * Memory Usage: 35.7 MB, less than 16.67% of JavaScript online submissions for First Missing Positive.
+ * Runtime: 48 ms, faster than 96.82% of JavaScript online submissions for First Missing Positive.
+ * Memory Usage: 33.7 MB, less than 100.00% of JavaScript online submissions for First Missing Positive.
  */
 /**
  * FIRST MISSING POSITIVE: 41
@@ -25,29 +25,27 @@ var firstMissingPositive = function (nums) {
     if(nums.length === 0) return 1;
     let max = nums[0];
     for (let i = 0; i < nums.length; i++) {
-        if(nums[i] < 0 ){
-            nums = nums.slice(0,i).concat(nums.slice(i+1,nums.length));
-            i = -1;
-        }
         if(nums[i] > max ) max = nums[i];
     }
+    // console.log(`end of loop nums is: ${nums}`);
     if(nums.length === 0) return 1;
-    console.log(`end of loop max is: ${max}`);
     for(let i = 1; i <= max; i++){
         if(!(nums.includes(i))) return i
     }
     return max + 1; 
 };
 
-let ex1 = [1, 2, 0]; //2:
+let ex1 = [1, 2, 0]; //3:
 let ex2 = [3, 4, -1, 1]; //2
 let ex3 = [7, 8, 9, 11, 12] // 1
 let ex4 = [-7, -9, -11, 12] // 1
 
-// console.log(firstMissingPositive(ex1));
-// console.log();
-// console.log(firstMissingPositive(ex2));
-// console.log();
-// console.log(firstMissingPositive(ex3));
-// console.log();
+console.log(firstMissingPositive(ex1));
+console.log();
+console.log(firstMissingPositive(ex2));
+console.log();
+console.log(firstMissingPositive(ex3));
+console.log();
+console.log(firstMissingPositive(ex4));
+console.log();
 console.log(firstMissingPositive([0,1,2,3,5,-55,-88,-97,-1,4]));
