8f7ffb366606bb0b0879aa72ae70c71655fb8829
liamcclane <lia_mcclane@msn.com>
2020-06-05 11:08:00 -0700

    Friday algo

---
+++ easy/plusOne.js
@@ -0,0 +1,48 @@
+/**66. Plus One
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * 
+ * The digits are stored such that the most significant digit is at the head of the list, 
+ * and each element in the array contain a single digit.
+ * 
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ * 
+ * Example 1:
+ *      Input: [1,2,3]
+ *      Output: [1,2,4]
+ *      Explanation: The array represents the integer 123.
+ * 
+ * Example 2:
+ *      Input: [4,3,2,1]
+ *      Output: [4,3,2,2]
+ *      Explanation: The array represents the integer 4321.
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    digits[digits.length - 1]++;
+    for(let i = digits.length - 1; i >= 0; i--) {
+        if(digits[i] === 10) {
+            digits[i] = 0;
+            if(i===0){
+                digits.unshift(1);
+            } else {
+                digits[i-1]++;
+            }
+        }
+    }
+    return digits;
+};
+console.log(plusOne([7,8,9]));
+console.log(plusOne([9,9]));
+
+// i think it's important to end with a nine on one example because in that case, 
+// we change the index before value
+// you're right!!
+// I think after we increase, loop thought to see if any of them are === 10
+// if they are change the one in front of it.... 
+// which might be tricky with 999... agreed
+// can we use a if statement for yhe last index and if it 9 start moving backwards? Or, will that invoke several if's?
+
+// I like the move backwards thing.... (thumbs up)
+// lol YES Perf
+// we'll be in contact and keep at it. Today Felt good 
--- medium/containerWithMostWater.js
+++ medium/containerWithMostWater.js
@@ -1,4 +1,4 @@
-/**
+/**11. Container With Most Water** 
  * Given n non-negative integers a1, a2, ..., an , 
  * where each represents a point at coordinate (i, ai). n 
  * vertical lines are drawn such that the two endpoints of line i 
@@ -17,19 +17,83 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let max = 0;
-    let len = height.length, j = len;
-    for(let i = 0; i < len; i++){
-        
+var maxArea = function (height) {
+    let amountOfWater = 0;
+    let len = height.length;
+    /**
+     * Pass in two heights from the given array and the difference of there
+     * indexes to calculate the amount of water held between those two
+     * @param {number} height1 
+     * @param {number} height2 
+     * @param {number} widthDiff 
+     * @returns {number}
+     */
+    function calculateAmountOfWater(height1, height2, widthDiff) {
+        return Math.min(height1, height2) * widthDiff;
     }
+    
+    for(let i = 0; i < len; i++) {
+        for(let j = i+1; j < len;j++) {
+            let y = calculateAmountOfWater(height[i],height[j],Math.abs(i-j));
+            if(y > amountOfWater) {
+                amountOfWater = y;
+            }
+        }
+    }
+    return amountOfWater;
 };
-function helper() {
 
-}
-let ex1 = [1,8,6,2,5,4,8,3,7];
+let ex1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 console.log(maxArea(ex1)) // 49 
 /**
  * because height[1] = 8 and height[8] = 7
  * and (8-1) * 7 = 49
  */
+let ex2 = [1, 2, 1];
+console.log(maxArea(ex2));
+/**
+ * because height[0] = 1 and height[2] = 1
+ * and (2-0) * 1 = 2
+ * 
+ * RETURNS 2
+ */
+
+/**
+ * Runtime: 788 ms, faster than 12.15% of JavaScript online submissions for Container With Most Water.
+ * Memory Usage: 36.8 MB, less than 21.55% of JavaScript online submissions for Container With Most Water.
+ */
+
+ /**
+  * 
+    // taking into account the case where we shouldn't be calculating according to the max in the array
+    // example [1,2,1] should return 2
+
+    // sorry, what are we returning?? The max area? Cool. I think I get it. 
+    // So, we want to the largest values in the array, that are also the furthest apart? YES basically, while still
+    // optimizing...
+    // the amount of squares that would be water, ya I have the visual in my files too. 
+
+    // with the above code I have to first test case down, but now need to think about [1,2,1] return 2 and not 1.
+
+  * Dead code from when I just wrote thinking about the only example given
+  * 
+  * refactored with nested for loop, giving a o(n^2) solution above.
+  * 
+  * Inefficient, but gets the job done... will refactor later.
+  * 
+  * // let firstMax = { ind: 0, val: height[0] };
+    // for (let i = 0; i < len; i++) {
+    //     if (firstMax.val < height[i]) {
+    //         firstMax.ind = i;
+    //         firstMax.val = height[i];
+    //     }
+    // }
+    // console.log(firstMax);
+    // for (let i = 0; i < height.length; i++) {
+    //     let x = calculateAmountOfWater(firstMax.val, height[i], Math.abs(firstMax.ind - i));
+    //     if (x > amountOfWater) {
+    //         amountOfWater = x;
+    //     }
+    // }
+    // console.log(amountOfWater);
+  */
