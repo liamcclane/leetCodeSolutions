eb51fe713ab2b37b51013cf44ca6ebda1a6c83be
liamcclane <lia_mcclane@msn.com>
2020-08-01 11:21:50 -0700

    daily Algo Sat

--- JavaAlgos/easy/NumberOfEquivalentDominoPairs.java
+++ JavaAlgos/easy/NumberOfEquivalentDominoPairs.java
@@ -3,12 +3,11 @@ package JavaAlgos.easy;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class NumberOfEquivalentDominoPairs {
 
     public int doStuff(int[][] dominoes) {
         int max = 0;
-        HashMap<int[], int> dict = new HashMap<int[], int>();
+        HashMap<int[], Integer> dict = new HashMap<int[], Integer>();
         for (int[] val : dominoes) {
             if (val[0] < val[1]) {
                 int temp = val[0];
@@ -18,31 +17,27 @@ public class NumberOfEquivalentDominoPairs {
         }
         for (int[] val : dominoes) {
             if (dict.containsKey(val)) {
-                // Integer old = dict.get(val);
-                // int w = old.intValue() + 1;
-                // dict.replace(val, w);
-                dict.get(val)++;
+                System.out.println("inside if ");
+                Integer o = dict.get(val);
+                int increase = Integer.valueOf(o);
+                increase++;
+                Integer n = increase;
+                dict.replace(val, o, n);
             } else {
-                // Integer i = 1;
-                // dict.put(val, i);
-                dict.put(val, 1);
+                System.out.println("inside else ");
+                System.out.println(val);
+                Integer i = 1;
+                dict.put(val, i);
             }
         }
-        // for (Integer i : dict.values()) {
-        //     System.out.println(i);
-        //     if (max < i.intValue()) {
-        //         max = i.intValue();
-        //     }
-        // }
-        for (int i : dict.values()) {
-            System.out.println(i);
-            if (max < i) {
-                max = i;
-            }
-        }
-        
-        System.out.println("**answer below**");
 
+        for (Integer i : dict.values()) {
+            if (max < Integer.valueOf(i)) {
+                max = Integer.valueOf(i);
+            }
+            System.out.println(" i is line 36");
+            System.out.println(i);
+        }
         // for (int i = 1; i < dominoes.length; i++) {
         // int[] frontVal = dominoes[i];
         // int[] Val = dominoes[i - 1];
@@ -50,12 +45,13 @@ public class NumberOfEquivalentDominoPairs {
         // count++;
         // }
         // }
+        System.out.println("**answer below**");
         return Integer.valueOf(max);
     }
 
     public static void main(String[] args) {
         NumberOfEquivalentDominoPairs n = new NumberOfEquivalentDominoPairs();
         int[][] par = { { 1, 2 }, { 1, 2 }, { 1, 1 }, { 1, 2 }, { 2, 2 } };
-        System.out.println(n.doStuff(par));
+        System.out.println(n.doStuff(par)); // this should be 3
     }
 }
