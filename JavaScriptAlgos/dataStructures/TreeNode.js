a8e1e0b5a21d969800cc338edee6f309cf937ab3
liamcclane <lia_mcclane@msn.com>
2020-08-01 11:20:02 -0700

    updated class

--- JavaScriptAlgos/dataStructures/TreeNode.js
+++ JavaScriptAlgos/dataStructures/TreeNode.js
@@ -7,4 +7,18 @@ module.exports = class TreeNode {
         this.left = (left === undefined ? null : left)
         this.right = (right === undefined ? null : right)
     }
+    // add = (val) => {
+    //     if (!this.right && !this.left) {
+    //         this.left = new TreeNode(val);
+    //         return this;
+    //     }
+    //     if (this.left && !this.right) {
+    //         this.right = new TreeNode(val);
+    //         return this;
+    //     }
+    //     // let curr = this;
+    //     // while(!curr) {
+    //     //     curr = curr.left;
+    //     // }
+    // }
 };
