b0be2d6d37346314293806f437ac251ead2d229b
liamcclane <lia_mcclane@msn.com>
2020-08-21 12:06:33 -0700

    added print statements

--- JavaScriptAlgos/easy/isPowerOfTwoThreeFour.js
+++ JavaScriptAlgos/easy/isPowerOfTwoThreeFour.js
@@ -18,12 +18,19 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfTwo = function(n) {
-    if(n===0) return false;
+var isPowerOfTwo = function (n) {
+    if (n === 0) return false;
     let x = Math.round(Math.log2(n));
-    if(2**x === n) return true
+    if (2 ** x === n) return true
     return false;
 };
+// for (let i = 1; i <= 64; i++) {
+//     if (isPowerOfTwo(i)) {
+//         console.log(isPowerOfTwo(i), i);
+//     } else {
+//         console.log("\t", isPowerOfTwo(i), i);
+//     }
+// }
 /**
  * Given an integer, write a function to determine if it is a power of three.
  * 
@@ -46,14 +53,21 @@ var isPowerOfTwo = function(n) {
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfThree = function(n) {
-    if(n===0) return false;
-    while(n>=1){
-        if(n===1) return true;
-        n = n/3;
+var isPowerOfThree = function (n) {
+    if (n === 0) return false;
+    while (n >= 1) {
+        if (n === 1) return true;
+        n = n / 3;
     }
     return false
 };
+// for (let i = 1; i <= 64; i++) {
+//     if (isPowerOfThree(i)) {
+//         console.log(isPowerOfThree(i), i);
+//     } else {
+//         console.log("\t", isPowerOfThree(i), i);
+//     }
+// }
 /**
  * Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
  * 
@@ -68,13 +82,20 @@ var isPowerOfThree = function(n) {
  * @param {number} num
  * @return {boolean}
  */
-var isPowerOfFour = function(num) {
-    if(num===0) return false;
+var isPowerOfFour = function (num) {
+    if (num === 0) return false;
     let x = Math.round(Math.log2(num));
-    if(x%2==1){ 
+    if (x % 2 == 1) {
         return false;
-    } else {
-        if(2**x === num) return true    
+    } else if (2 ** x === num) {
+        return true;
     }
     return false;
 };
+// for (let i = 1; i <= 64; i++) {
+//     if (isPowerOfFour(i)) {
+//         console.log(isPowerOfFour(i), i);
+//     } else {
+//         console.log("\t", isPowerOfFour(i), i);
+//     }
+// }
