cd9cfdcee400ce4c4d090a794432711cb7bde184
liamcclane <lia_mcclane@msn.com>
2020-08-31 14:34:46 -0700

    add description

--- JavaScriptAlgos/easy/twoSums.js
+++ JavaScriptAlgos/easy/twoSums.js
@@ -1,16 +1,25 @@
-// Lia McClane
-// Level: easy
-
-
-// Given an array of integers, return indices of the two numbers such that
-//  they add up to a specific target.
-// You may assume that each input would have exactly one solution, 
-// and you may not use the same element twice.
-// Given nums = [2, 7, 11, 15], target = 9,
-
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
-/**
+/**1. Two Sum
+ * Given an array of integers nums and and integer target, return the indices of the 
+ * two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and 
+ * you may not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1]
+ * Example 2:
+ * 
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ * 
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -23,4 +32,14 @@ var twoSum = function (nums, target) {
             }
         }
     }
+    /**
+     * Runtime: 108 ms
+     * Memory Usage: 34.7 MB
+     * Your runtime beats 52.66 % of javascript submissions.
+     * Your memory usage beats 95.81 % of javascript submissions.
+     */
 };
+/**tags for later look up
+ * refactorMe
+ * nestedForLoops
+ */
