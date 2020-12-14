aee8521e3b61ce5756424bb3f1888ae39ec453b1
liamcclane <lia_mcclane@msn.com>
2020-12-14 14:57:57 -0800

    leetcode 445 addtwonumbersII link list completed

--- JavaScriptAlgos/medium/addTwoNumbersIILinkedList.js
+++ JavaScriptAlgos/medium/addTwoNumbersIILinkedList.js
@@ -5,7 +5,20 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-/**
+/**445. Add Two Numbers II
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The most significant digit comes first and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * 
+ * Follow up:
+ * What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+ * 
+ * Example:
+ * 
+ * Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 8 -> 0 -> 7
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
@@ -34,9 +47,17 @@ var addTwoNumbers = function (l1, l2) {
         }
         return count;
     }
+
+    // variable declarations
     let count1 = lenOfList(l1), count2 = lenOfList(l2),
         largerList = l1, smallerList = l2,
         answerList, answerRunner;
+    
+    /**
+     * helper method that moves though both list at the same 
+     * time once the pointers are at the same tens 
+     * value in the number
+     */
     const addLists = () => {
         while (largerList != null) {
             let newestNode = new ListNode(largerList.val + smallerList.val);
@@ -52,6 +73,11 @@ var addTwoNumbers = function (l1, l2) {
             smallerList = smallerList.next;
         }
     }
+    /**
+     * helper method that moves though the lager size list 
+     * and adds to the answerList until both pointers are at the
+     * same tens value
+     */
     const fillLargerOverFlow = () => {
         while (count1 > count2) {
             let newNode = new ListNode(largerList.val);
@@ -67,6 +93,10 @@ var addTwoNumbers = function (l1, l2) {
         }
     }
 
+    /**
+     * this helper function gets called if list2 size is greater
+     * than list1 and we need to switch the variables
+     */
     const adjustVarNames = () => {
         largerList = l2;
         smallerList = l1;
@@ -75,24 +105,37 @@ var addTwoNumbers = function (l1, l2) {
         count2 = temp;
     }
 
-    const reverseList = function (head) {
-        let previous = null;
-        while (head !== null) {
-            let next = head.next;
-            head.next = previous;
-            previous = head
-            head = next;
+    /**
+     * reverses the answerList once both l1 and l2
+     * have been added together, this is was done 
+     * to be able to easily carry over values >= 10
+     * into the answerList
+     * 
+     * it will be called twice, once to reverse it and 
+     * then again to reverse it back to normal
+     */
+    const reverseList = () => {
+        let back = null, front = answerList.next;
+        while(answerList != null) {
+            answerList.next = back;
+            back = answerList;
+            answerList = front;
+            front = front != null ? front.next : null;
         }
-        return previous;
-    };
+        answerList = back;
+    }
 
+    /**
+     * this helper method iterates though a reversed answerList
+     * and carries over the tens columns in addition
+     */
     const carryOverTens = () => {
         let runner = answerList;
         while (runner != null) {
-            if (runner.val > 10) {
+            if (runner.val >= 10) {
                 runner.val -= 10;
                 if (runner.next === null) {
-                    runner.next.next = new ListNode(1);
+                    runner.next = new ListNode(1);
                 } else {
                     runner.next.val++;
                 }
@@ -101,126 +144,18 @@ var addTwoNumbers = function (l1, l2) {
         }
     }
 
-
     // switch the names of larger and smaller
     if (count2 > count1) {
         adjustVarNames();
     }
     fillLargerOverFlow();
-    // console.log("answerList", answerList);
-    // console.log("answerRunner", answerRunner);
-    // console.log("largerList.val", largerList.val);
     addLists();
     reverseList(answerList);
     carryOverTens();
     reverseList(answerList);
-
-    // if (answerList.val >= 10) {
-    //     let temp = answerList;
-    //     answerList = new ListNode(1);
-    //     temp.val = temp.val - 10;
-    //     answerList.next = temp;
-    // }
-
-    // back to the top, and carry over vals > 10
-    // answerRunner = answerList;
-    // let front = answerRunner.next;
-
-    // while (front != null) {
-
-    //     if (front.val >= 10) {
-    //         answerRunner.val++;
-    //         front.val = front.val - 10;
-    //     }
-
-    //     front = front.next;
-    //     answerRunner = answerRunner.next;
-    // }
-
     return answerList;
+    /**
+     * Runtime: 140 ms, faster than 57.62% of JavaScript online submissions for Add Two Numbers II.
+     * Memory Usage: 44.2 MB, less than 75.84% of JavaScript online submissions for Add Two Numbers II.
+     */
 };
-/**
- * if(l1 === null) {
-        return l2;
-    } else if (l2 === null) {
-        return l1;
-    } else if (l1 === null && l2 === null) {
-        return new ListNode(0);
-    }
-    
-    const lenOfList = (list, count = 0) => {
-        let runner = list;
-        while(runner != null) {
-            count++;
-            runner = runner.next;
-        }
-        return count;
-    }
-    
-    let count1 = lenOfList(l1), count2 = lenOfList(l2),
-        largerList = l1, smallerList = l2;
-    
-    if(count2 > count1) {
-        largerList = l2;
-        smallerList = l1;
-        let temp = count1; 
-        count1= count2;
-        count2 = temp;
-    }
-    
-    let answerList, answerRunner;
-    
-    while(count1 > count2) {
-        let newNode = new ListNode(largerList.val);
-        if(answerList === undefined) {
-            answerList = newNode;
-            answerRunner = answerList;
-        } else {
-            answerRunner.next = newNode;
-            answerRunner = answerRunner.next;
-        }
-        largerList = largerList.next;
-        count1--;
-    }
-//     console.log("answerList",answerList);
-//     console.log("answerRunner", answerRunner);
-//     console.log("largerList.val", largerList.val);
-    
-    while(largerList != null) {
-        let newestNode = new ListNode(largerList.val + smallerList.val);
-        // console.log(answerList);
-        if(answerList === undefined) {
-            answerList = newestNode;
-            answerRunner = answerList;
-        } else {
-            answerRunner.next = newestNode;
-            answerRunner = answerRunner.next;
-        }
-        largerList = largerList.next;
-        smallerList = smallerList.next;
-    }
-    
-    if(answerList.val >= 10) {
-        let temp = answerList;
-        answerList = new ListNode(1);
-        temp.val = temp.val - 10;
-        answerList.next = temp;
-    }
-    
-    // back to the top, and carry over vals > 10
-    answerRunner = answerList;
-    let front = answerRunner.next;
-    
-    while(front != null) {
-        
-        if(front.val >= 10) {
-            answerRunner.val++;
-            front.val = front.val - 10;
-        }
-        
-        front = front.next;
-        answerRunner = answerRunner.next;
-    }
-    
-    return answerList;
- */
