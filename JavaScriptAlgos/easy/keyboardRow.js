756cefe82655436eb907d7eb743f85541fed3348
Lia McClane-RazerLaptop <lia_mcclane@msn.com>
2021-08-22 21:49:12 -0700

    Daily Algo 	Keyboard Row, 500

--- JavaScriptAlgos/easy/keyboardRow.js
+++ JavaScriptAlgos/easy/keyboardRow.js
@@ -1,4 +1,42 @@
 /**Keyboard Row-500
+ * Given an array of strings words, return the words that can be 
+ * typed using letters of the alphabet on only one row of American keyboard like the image below.
+ * 
+ * In the American keyboard:
+ * 
+ * the first row consists of the characters "qwertyuiop",
+ * the second row consists of the characters "asdfghjkl", and
+ * the third row consists of the characters "zxcvbnm".
+ * @param {string[]} words
+ * @return {string[]}
+ */
+ var findWords2 = function(words) {
+    
+    const dict = {};
+    
+    const fillDict = arr => {
+        for(let i = 0; i < arr.length; i++) {
+            for(let j = 0; j < arr[i].length; j++) {
+                dict[arr[i].charAt(j).toString()] = i;       
+            }
+        }
+    }
+    
+    fillDict(["qwertyuiop","asdfghjkl","zxcvbnm"]);
+    
+    const singleLine = word => {
+        for(let i = 0, j = 1; j < word.length; i++, j++) {
+            if(dict[word.charAt(i).toLowerCase()] != dict[word.charAt(j).toLowerCase()]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    return words.filter(word => singleLine(word));
+};
+
+/**Keyboard Row-500
  * Given a List of words, 
  * return the words that can be typed using letters of alphabet on only one row's of American keyboard
  * @param {string[]} words
